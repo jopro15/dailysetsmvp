d33123e3175998128e36bcea4cc91608b55730a5
jopro15 <50751485+jopro15@users.noreply.github.com>
2021-09-20 19:39:09 +0200

    Updates

---
+++ precache-manifest.9ef1bb52c72a47038b362108610d1a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3474e85de47adeb37a8f7bb2f703652f",
+    "url": "/dailysetsmvp/index.html"
+  },
+  {
+    "revision": "64e6aa762e431100f1d1",
+    "url": "/dailysetsmvp/static/css/main.518413b2.chunk.css"
+  },
+  {
+    "revision": "9621ab61d3663eb42f05",
+    "url": "/dailysetsmvp/static/js/2.397537e5.chunk.js"
+  },
+  {
+    "revision": "64e6aa762e431100f1d1",
+    "url": "/dailysetsmvp/static/js/main.43c59e6e.chunk.js"
+  },
+  {
+    "revision": "2332a5ca86f407d38a02",
+    "url": "/dailysetsmvp/static/js/runtime-main.270ca18b.js"
+  },
+  {
+    "revision": "5d9dceeba04e65ca53da3114a2aeabc3",
+    "url": "/dailysetsmvp/static/media/Communityicon.5d9dceeb.png"
+  },
+  {
+    "revision": "fe75ba231ecc62202f095165965b6ed4",
+    "url": "/dailysetsmvp/static/media/logo.fe75ba23.png"
+  }
+]);
